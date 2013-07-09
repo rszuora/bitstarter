d034230e19cdc4ba4d40a18e35e24549706b512c
rszuora <richard.sawey@zuora.com>
2013-07-09 02:43:31 +0000

    Fixed download problem

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buff = new Buffer(fs.readFileSync('./index.html', 'utf8'), "utf8");
-  response.send(buff.toString);
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
